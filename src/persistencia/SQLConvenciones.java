0c1dd26e98a136728955e2262d010ffbdd0ae230
JUAN DAVID DIAZ CRISTANCHO <jd.diazc@sis.virtual.uniandes.edu.co>
2019-05-14 19:56:34 -0500

    ho

--- src/persistencia/SQLConvenciones.java
+++ src/persistencia/SQLConvenciones.java
@@ -1,5 +1,8 @@
 package persistencia;
 
+
+
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.jdo.PersistenceManager;
@@ -42,12 +45,47 @@ public class SQLConvenciones {
 	public String eliminarConvencion(PersistenceManager pm,long id)
 	{
 		String respuesta = "";
-		
+		try{
+			
+			String lel = "SELECT c.NOMBRE,p.VALOR " + "FROM ((CLIENTES c INNER JOIN GASTOS g ON c.ID_RESERVA_HABITACION=g.ID_RESERVA_HABITACION AND c.ID_CONVENCION="+id+")" 
+					+" INNER JOIN PRODUCTOS p ON g.ID_PRODUCTO=p.ID) LEFT OUTER JOIN RESERVAS_CONVENCIONES rc ON p.ID_SERVICIO=rc.ID_SERVICIO WHERE rc.ID_SERVICIO IS NULL"
+					+" ORDER BY c.ID_RESERVA_HABITACION ";
+			System.out.println(lel);
+			Query q = pm.newQuery(SQL,lel);
+			System.out.println("LLEGA EL PELUDO");
+			List<Object> objects = q.executeList();
+			String autorAct = "";
+			Double tot = 0.0;
+			for (Object o : objects) {
+				Object[] datos = (Object[]) o;
+//				System.out.println(datos[0]);
+//				System.out.println(datos[1]);
+				if(!datos[0].equals(autorAct)){
+					respuesta+=autorAct+" DEBE PAGAR "+tot +"\n";
+					autorAct=datos[0].toString();
+					tot=0.0;
+				}
+				tot+=((BigDecimal)datos[1]).longValue();
+			}
+			System.out.println(respuesta);
+			//List<Object> objects = q.executeList();
+			System.out.println("LLEGA AUN MAS LEJOS");
+//			Object[] c = objects.toArray();
+			
+//			for(int e=0;e<objects.size();e++){
+//				System.out.println(objects.get(e)+"");
+//			}
+			System.out.println("CHOLE?");
+			
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 		
 //		Query q = pm.newQuery(SQL, "UPDATE CONVENCIONES SET PAZ_Y_SALVO = 1 WHERE id = ?");
 //        q.setParameters(id);
 //        q.executeUnique();
-        return "";    
+        return respuesta;    
 	}
 	
 	public Convencion darConvencion(PersistenceManager pm, long id)
